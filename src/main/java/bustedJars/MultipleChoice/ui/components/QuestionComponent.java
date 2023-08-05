00ef137253238f49b833a707856beffa1a35d50a
A.V <goddel@gmail.com>
2023-08-05 19:08:31 +0300

    Text element

--- src/main/java/bustedJars/MultipleChoice/ui/components/QuestionComponent.java
+++ src/main/java/bustedJars/MultipleChoice/ui/components/QuestionComponent.java
@@ -3,12 +3,15 @@ package bustedJars.MultipleChoice.ui.components;
 import bustedJars.MultipleChoice.core.Anwser;
 import bustedJars.MultipleChoice.core.Question;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class QuestionComponent extends VerticalLayout {
 
@@ -17,18 +20,29 @@ public class QuestionComponent extends VerticalLayout {
 
 
     public QuestionComponent(Question question) {
-        this.question= new H1(question.question());
+        this.question = new H1(question.question());
         add(this.question);
         setAnwserComponents(question.anwsers());
         setAlignItems(Alignment.CENTER);
         getStyle().set("border", "2px solid black");
     }
-    private void setAnwserComponents(Set<Anwser> anwserSet){
-        List<SelectEntryComponent> list= new ArrayList<>();
-        anwserSet.stream().forEach(anwser -> list.add(new SelectEntryComponent(anwser.anwser())));
+
+    private void setAnwserComponents(Set<Anwser> anwserList) {
+        List<SelectEntryComponent> list = new ArrayList<>();
+        AtomicInteger ordinal = new AtomicInteger(1);
+        anwserList.stream().forEach(anwser -> {
+            String value = anwser.anwser();
+            list.add(new SelectEntryComponent(value));
+        });
         Collections.shuffle(list);
-        this.anwserEntries =list;
-        this.anwserEntries.stream().forEach(entry-> add(entry));
+        this.anwserEntries = list;
+        this.anwserEntries.stream().forEach(entry -> {
+            HorizontalLayout horizontalLayout = new HorizontalLayout();
+            horizontalLayout.add(new H2(ordinal + ") "));
+            horizontalLayout.add(entry);
+            add(horizontalLayout);
+            ordinal.getAndIncrement();
+        });
     }
 
     public H1 getQuestion() {
--- src/main/java/bustedJars/MultipleChoice/ui/components/SelectEntryComponent.java
+++ src/main/java/bustedJars/MultipleChoice/ui/components/SelectEntryComponent.java
@@ -1,25 +1,38 @@
 package bustedJars.MultipleChoice.ui.components;
 
+import com.vaadin.flow.component.HasText;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.dom.Style;
 
-public class SelectEntryComponent  extends Button {
-     private String  display;
-     private boolean isSelected= false;
+public class SelectEntryComponent extends HorizontalLayout {
+    private String display;
+    private boolean isSelected = false;
+
     public SelectEntryComponent(String display) {
-        this.display=display;
-        setText(display);
+        this.display = display;
+        Text displayText = new Text(display);
+        add(displayText);
+        setAlignItems(Alignment.CENTER);
         getStyle().set("background-color", "white");
         setWidth("600px");
+        setMaxWidth("600px");
+        getStyle().set("padding", "5px");
+
+
         addClickListener(e -> {
-            isSelected= isSelected?false:true;
-            if(isSelected){
+            isSelected = isSelected ? false : true;
+            if (isSelected) {
                 getStyle().set("background-color", "green");
-            }else{
+            } else {
                 getStyle().set("background-color", "white");
             }
         });
         getStyle().set("border", "1px solid gray");
     }
+
     public boolean isSelected() {
         return isSelected;
     }
